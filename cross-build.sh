93d93675dcef1c23f681868e0fb9c6a01af943d0
arabcoders <admin@arabcoders.org>
2025-05-17 18:30:33 +0300

    fixes

--- cross-build.sh
+++ cross-build.sh
@@ -1,5 +1,14 @@
+# Register QEMU (just once per host—skip if you already did it)
+# docker run --rm --privileged tonistiigi/binfmt --install all
+
+docker buildx create --use --name tmp-multiarch
+
 docker buildx build \
-  --platform linux/amd64,linux/arm64 \
-  --tag ghcr.io/arabcoders/franken_builder:latest \
-  --push \
-  .
+        --builder tmp-multiarch \
+        --platform linux/amd64,linux/arm64 \
+        --tag ghcr.io/arabcoders/franken_builder:latest \
+        --progress plain \
+        --push \
+        . &&
+
+docker buildx rm tmp-multiarch
